078622bcb9257183700c0847a36884a45de6cd1f
jonathancardoso <jonathancardoso@outlook.com>
2015-10-29 16:18:10 +0000

    Added q_vals

--- R/multired.R
+++ R/multired.R
@@ -44,9 +44,31 @@ read_multilayer <- function(filename, n, networkName=NULL) {
     multilayer$name <- c("name"=networkName)
     class(multilayer) <- "multilayer"
     
-    multilayer$adjacencies <- lapply(1:multilayer$noLayers,get_adjacency,multilayer)
+    #Store adjacency matrices
+    multilayer$adjacencies <- lapply(1:multilayer$noLayers,get_adjacency_of_layer,multilayer)
     
     multilayer
+}
+
+#' Converts a List of Matrix to Data frame
+#' 
+#' Each position in the list must be a n x n matrix
+#' 
+#' @export
+list_to_multilayer <- function(l, n, networkName=NULL){
+  multilayer <- list()
+  multilayer$edgelist <- NA
+  multilayer$noLayers <- length(l)
+  if (missing(n)) {
+    warning("Number of nodes were not informed, inferring from the list of adjacencies.",call. = F)
+    multilayer$n <- c("noNodes" = dim(l[[1]])[1])
+  } else {
+    multilayer$n <- c("noNodes"=n)
+  }
+  multilayer$adjacencies <- l
+  multilayer$name <- c("name"=networkName)
+  class(multilayer) <- "multilayer"
+  multilayer
 }
 
 #' @export
@@ -74,25 +96,27 @@ print.multilayer <- function(m,...) {
 #' @param m A multilayer object
 #' @return A \code{\link[Matrix]{Matrix}} object
 #' @export
-get_adjacency <- function(layerIds,m){
-  UseMethod("get_adjacency",m)
-}
-
-#' @export
-get_adjacency.multilayer <- function(layerIds, m) {
+get_adjacency_of_layer <- function(layerId,m){
+  if(!inherits(m,"multilayer")){
+    stop("m must be a multilayer object.")
+  }
     A <- Matrix(0, nrow = m$n, ncol = m$n, sparse = TRUE)
-    el <- subset(m$edgelist, layerID %in% layerIds)
-    # Summing weights in all layers
-    el <- ddply(el, .(source, target), summarize, weight = sum(weight))
-    el <- as.matrix(el)
-    tryCatch({
+    if(layerId <= m$noLayers & layerId > 0){
+      el <- subset(m$edgelist, layerID == layerId)
+      # Summing weights in all layers
+      el <- ddply(el, .(source, target), summarize, weight = sum(weight))
+      el <- as.matrix(el)
+      tryCatch({
         A[el[, 1:2]] <- el[, "weight"]
         # Sum existing value with the weight from the other link (make it bi-directional)
         A[el[, c(2, 1)]] <- A[el[, c(2, 1)]] + A[el[, c(1, 2)]]
-    }, error = function(e) {
+      }, error = function(e) {
         stop("Could not create adjacency matrix. Indices of nodes >= number of nodes informed.")
-    })
-    A
+      })
+      A
+    }else{
+     stop("Invalid layerId.") 
+    }  
 } 
 
 #' Adjacency matrix of one or more layers
@@ -117,10 +141,103 @@ get_layer_entropy <- function(A){
 
 #' @export
 get_entropy <- function(m){
-  UseMethod("get_entropy",m)
+  if(!inherits(m,"multilayer")){
+    stop("m must be a multilayer object.")
+  }
+  Reduce("+",Map(get_layer_entropy,m$adjacencies))
 }
 
-get_entropy.multilayer <- function(m){
-  Reduce("+",Map(get_layer_entropy,m$adjacencies))
+#' Entropy of the aggregation of all layers
+#' 
+#' @export
+get_agg_entropy <- function(m){
+  if(!inherits(m,"multilayer")){
+    stop("m must be a multilayer object.")
+  }
+  agg <- Reduce("+",m$adjacencies)
+  get_layer_entropy(agg)
+}
+
+#' Computes Jensen-Shannon matrix
+#'  @export
+compute_JSD_matrix <- function(m){
+  if(!inherits(m,"multilayer")){
+    stop("m must be a multilayer object.")
+  }
+  allComb <- t(combn(m$noLayers,2))
+  JSD <- Matrix(0,m$noLayers,m$noLayers)
+  for(idx in 1:nrow(allComb)){
+    i <- allComb[idx,1]
+    j <- allComb[idx,2]
+    aggr <- (m$adjacencies[[i]] + m$adjacencies[[j]])/2
+    JSD[i,j] <- sqrt(get_layer_entropy(aggr) - 0.5*(get_layer_entropy(m$adjacencies[[i]]) + get_layer_entropy(m$adjacencies[[j]])))
+    JSD[j,i] <- JSD[i,j]
+  }
+  JSD
+}
+
+#' Calculates relative entropy (q) for reduced multilayer network
+#' 
+#' Calculates the relative entropy of a reduced multilayer, 
+#' relative to the aggregated entropy of the original multilayer (not the reduced). 
+#' 
+#' @param reducedM the multilayer network with reduced number of layers
+#' @param aggEntropy the aggregated entropy of the original multilayer. See \code{\link{get_agg_entropy}}.
+#' @return The relative entropy (q)
+#' @export
+get_relative_entropy <- function(reducedM,aggEntropy){
+  if(!inherits(reducedM,"multilayer")){
+    stop("reducedM must be a multilayer object.")
+  }
+  HC <- get_entropy(reducedM)/reducedM$noLayers
+  1 - HC/aggEntropy
+}
+
+#' Find the reduced multilayer network
+#' 
+#' \code{find_reduced} is the main function of this package. 
+#' It reduces the multilayer network passed as an argument (\code{m}) and  
+#' 
+#' @export
+#' @examples
+#' \dontrun{
+#' m <- read_multilayer("test.txt")
+#' find_reduced(m)
+#' }
+find_reduced <- function(m){
+  if(!inherits(m,"multilayer")){
+    stop("m must be a multilayer object.")
+  }
+  aggEntropy <- get_agg_entropy(m)
+  
+  merges <- hclust(as.dist(compute_JSD_matrix(m)),method="ward.D2")$merge
+  noMerges <- nrow(merges)
+  
+  qVals <- vector(mode="numeric",length=noMerges)
+  adjMerges <- vector(mode="list",length=noMerges-1)
+  currMultilayer <- m$adjacencies
+  merged <- vector(mode="logical",length=m$noLayers)
+  
+  #Evaluate all merges
+  for(i in 1:noMerges){
+    layer1 <- merges[i,1]
+    layer2 <- merges[i,2]
+    
+    if(layer1 < 0){
+      merged[abs(layer1)] <- TRUE
+    }
+    if(layer2 < 0){
+      merged[abs(layer2)] <- TRUE
+    }
+    
+    adj1 <- if(layer1 < 0){m$adjacencies[[abs(layer1)]]}else{adjMerges[layer1][[1]]}
+    adj2 <- if(layer2 < 0){m$adjacencies[[abs(layer2)]]}else{adjMerges[layer2][[1]]}
+    adjMerges[[i]] <- adj1 + adj2
+    currMultilayer <- Reduce(append,m$adjacencies[which(!merged)],adjMerges[1:i])
+    
+    reducedM <- list_to_multilayer(currMultilayer,m$n,m$name)
+    qVals[i] <- get_relative_entropy(reducedM,aggEntropy)
+  }
+  qVals
 }
 
